b9927049b8501aefe3d646fe6beabacff8723b7f
Igor Amaral <9051547+igoramaral@users.noreply.github.com>
2025-02-21 12:03:06 -0300

    feature/GS9: characterController creation

---
+++ src/controllers/characterController.js
@@ -0,0 +1,71 @@
+const characterService = require('../services/characterService');
+
+const createCharacter = async (req, res) => {
+    const userId = req.userId;
+
+    try {
+        let char = await characterService.createCharacter(req.body, userId);
+
+        res.status(201).json(char);
+    } catch(err){
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
+
+const findCharacter = async (req, res) => {
+    const userId = req.userId;
+
+    try {
+        let char = await characterService.findCharacter(req.params.id, userId);
+
+        res.status(200).json(char);
+    } catch(err) {
+        if (err.message == "Personagem não encontrado"){
+            return res.status(404).json({ error: "Personagem não encontrado" });
+        } else {
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+}
+
+const updateCharacter = async (req, res) => {
+    const userId = req.userId;
+    const charId = req.params.id;
+    const charData = req.body
+
+    try {
+        let char = await characterService.updateCharacter(charId, userId, charData);
+
+        res.status(200).json(char);
+    } catch(err) {
+        if (err.message == "Personagem não encontrado"){
+            return res.status(404).json({ error: "Personagem não encontrado" });
+        } else {
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+}
+
+const deleteCharacter = async (req, res) => {
+    const userId = req.userId;
+    const charId = req.params.id;
+
+    try {
+        let char = await characterService.deleteCharacter(charId, userId);
+
+        res.status(200).json({ message: "Personagem deletado com sucesso" });
+    } catch(err) {
+        if (err.message == "Personagem não encontrado"){
+            return res.status(404).json({ error: "Personagem não encontrado" });
+        } else {
+            return res.status(500).json({ error: "Internal Server Error" });
+        }
+    }
+}
+
+module.exports = {
+    createCharacter,
+    findCharacter,
+    updateCharacter,
+    deleteCharacter
+}
