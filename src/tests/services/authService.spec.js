9d431486916ab90543687e91df0f794f8748bcec
Igor Amaral <9051547+igoramaral@users.noreply.github.com>
2025-03-10 16:18:03 -0300

    feat: unit tests for authService and authController

---
+++ src/tests/services/authService.spec.js
@@ -0,0 +1,63 @@
+const AuthService = require('../../services/authService');
+const User = require('../../models/user');
+const mockingoose = require('mockingoose');
+const jwt = require('jsonwebtoken');
+
+jest.mock('jsonwebtoken', () => ({
+    sign: jest.fn(() => 'mocked-jwt-token')
+}));
+
+describe('AuthService', () => {
+    beforeEach(() => {
+        mockingoose.resetAll();
+    });
+
+    describe('authService.login', () => {
+        it('should return token and user data when providing valid credentials', async () => {
+            const mockUser = new User({
+                _id: '64feba7fbc13adf42caa92a1',
+                email: 'test@example.com',
+                password: 'hashedpassword'
+            });
+
+            mockUser.checkPassword = jest.fn().mockResolvedValue(true);
+
+            mockUser.toObject = jest.fn().mockReturnValue({
+                _id: '64feba7fbc13adf42caa92a1',
+                email: 'test@example.com',
+                password: undefined // Para garantir que a senha não seja exposta
+            });
+
+            mockingoose(User).toReturn(mockUser, 'findOne');
+
+            const response = await AuthService.login('test@example.com', 'validpassword');
+
+            expect(response).toHaveProperty('token', 'mocked-jwt-token');
+            expect(response.user).toHaveProperty('id', '64feba7fbc13adf42caa92a1');
+            expect(response.user).not.toHaveProperty('_id');
+            expect(response.user).not.toHaveProperty('password');
+        });
+
+        it('should throw error in case of wrong credentials', async () => {
+            const mockUser = {
+                _id: '64feba7fbc13adf42caa92a1',
+                email: 'test@example.com',
+                password: 'hashedpassword', // Certifique-se de incluir o password
+                checkPassword: jest.fn().mockResolvedValue(false) // Simulando senha errada
+            };
+
+            mockingoose(User).toReturn(mockUser, 'findOne');
+
+            await expect(AuthService.login('test@example.com', 'wrongpassword'))
+                .rejects.toThrow('Senha incorreta');
+        });
+
+        it('should throw error in case of user not found', async () => {
+            mockingoose(User).toReturn(null, 'findOne');
+
+            await expect(AuthService.login('notfound@example.com', 'password'))
+                .rejects.toThrow('Usuário não encontrado');
+        });
+
+    });
+});
