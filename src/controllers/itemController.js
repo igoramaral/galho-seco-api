59afa44c92b777c477500f88151179c4a618741c
Igor Amaral <9051547+igoramaral@users.noreply.github.com>
2025-07-11 11:45:48 -0300

    added updateCharacterDerivedFields function to update relevant info on character/item creation

--- src/controllers/itemController.js
+++ src/controllers/itemController.js
@@ -1,5 +1,7 @@
 const itemService = require('../services/itemService');
 const Character = require('../models/character');
+const MissingKeyError = require("../errors/missingKeyError");
+const updateCharacterDerivedFields = require('../utils/updateCharacterDerivedFields');
 
 class ItemController {
     async createItem(req, res) {
@@ -11,26 +13,24 @@ class ItemController {
                 { $addToSet: { items: item._id } }
             );
 
+            await updateCharacterDerivedFields(req.params.characterId, req.userId);
+
             res.status(201).json(item);
         } catch (err) {
-            if (err instanceof MissingKeyError) {
-                res.status(err.statusCode).json({ 
-                    error: err.name,
-                    field: err.field,
-                    message: err.message
-                });
-            } else {
-                if (err.message === "Personagem não encontrado"){
-                    res.status(404).json({ error: err.message});
-                }else {
-                    res.status(500).json({ error: "Internal Server Error" })
-                } 
-            }
+            if (err.message === "Personagem não encontrado."){
+                res.status(404).json({ error: err.message});
+            }else if (err.message === "type é um campo obrigatório" || err.message.includes("item desconhecido")) {
+                res.status(400).json({ error: err.message });
+            }else {
+                res.status(500).json({ error: "Internal Server Error" })
+            } 
         }
     }
 
+
     async createManyItems(req, res) {
         try {
+            console.log(req.body);
             const items = [];
             for (const data of req.body) {
                 const item = await itemService.createItem(data, req.params.characterId);
@@ -42,14 +42,18 @@ class ItemController {
                 { _id: req.params.characterId },
                 { $addToSet: { items: { $each: itemIds } } }
             );
+
+            await updateCharacterDerivedFields(req.params.characterId, req.userId);
             
             res.status(201).json(items);
         } catch (err) {
             if (err.message === "Personagem não encontrado"){
                 res.status(404).json({ error: err.message});
+            }else if (err.message === "type é um campo obrigatório" || err.message.includes("item desconhecido")) {
+                res.status(400).json({ error: err.message });
             }else {
                 res.status(500).json({ error: "Internal Server Error" })
-            }
+            } 
         }
     }
 
@@ -59,7 +63,7 @@ class ItemController {
 
             res.status(200).json(updated);
         } catch (err) {
-            if (err.message === "Personagem não encontrado"){
+            if (err.message === "Item não encontrado"){
                 res.status(404).json({ error: err.message});
             }else {
                 res.status(500).json({ error: "Internal Server Error" })
@@ -76,7 +80,21 @@ class ItemController {
                 { $pull: { items: req.params.itemId } }
             );
 
+            await updateCharacterDerivedFields(req.params.characterId, req.userId);
+
             res.status(200).json({ message: "Item excluído com sucesso" });
+        } catch (err) {
+            res.status(404).json({ error: err.message });
+        }
+    }
+
+    async getAllItems(req, res) {
+        try {
+            const items = await itemService.getAllItems(req.params.characterId);
+
+            await updateCharacterDerivedFields(req.params.characterId, req.userId);
+
+            res.status(200).json(items)
         } catch (err) {
             res.status(404).json({ error: err.message });
         }
