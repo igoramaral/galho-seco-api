0729a472a335a4761555202761fc5c4f5be7b0f4
Igor Amaral <9051547+igoramaral@users.noreply.github.com>
2025-07-11 11:46:18 -0300

    added treatment to allow bigger request body size

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUI = require('swagger-ui-express');
 const swaggerSpec = require('./swagger')
+const bodyParser = require('body-parser');
 
 //Importing Routes
 const userRoutes = require('./routes/userRoutes');
@@ -12,7 +13,8 @@ const itemRoutes = require('./routes/itemRoutes');
 
 // initializing the application
 const app = express();
-app.use(express.json());
+// app.use(express.json());
+app.use(bodyParser.json({limit: '2mb'}))
 
 //swagger documnentation
 app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec,{
@@ -25,7 +27,7 @@ app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec,{
 app.use('/api/v1', userRoutes);
 app.use('/api/v1', authRoutes);
 app.use('/api/v1', characterRoutes);
-app.use('/api/v1/character/:id', characterRoutes);
+app.use('/api/v1', itemRoutes);
 
 // Start the server
 mongoose
